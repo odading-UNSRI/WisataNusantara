d3533e552ca8bbb47934c58cf49a09825b6eb8f6
Royan Dwi Saputra <royan.dwisaputra@gmail.com>
2020-11-17 19:55:29 +0700

    UPDATE

--- app/src/main/java/com/example/wisatanusantara/main.java
+++ app/src/main/java/com/example/wisatanusantara/main.java
@@ -1,59 +1,59 @@
 package com.example.wisatanusantara;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import android.view.View;
-import android.widget.ImageView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.wisatanusantara.adapter.WisataAdapter;
+import com.example.wisatanusantara.model.RoodPariwisataModel;
+import com.example.wisatanusantara.model.WisataItem;
+import com.example.wisatanusantara.rest.ApiConfig;
+import com.example.wisatanusantara.rest.ApiService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class main extends AppCompatActivity {
-
-    private ImageView ivAgrowisata;
-    private ImageView ivAlam;
-    private ImageView ivBudaya;
-    private ImageView ivEdukasi;
-    private ImageView ivKawasanWisataTerpadu;
-    private ImageView ivKolamRenang;
-    private ImageView ivLainLain;
-    private ImageView ivMinatKhusus;
-    private ImageView ivReligi;
-    private ImageView ivTaman;
-    private CardView cvAgrowisata;
-    private CardView cvAlam;
-    private CardView cvBudaya;
-    private CardView cvEdukasi;
-    private CardView cvKawasanWisataTerpadu;
-    private CardView cvKolamRenang;
-    private CardView cvLainLain;
-    private CardView cvMinatKhusus;
-    private CardView cvReligi;
-    private CardView cvTaman;
+    private RecyclerView recyclerView;
+    private ArrayList<WisataItem> wisataItems;
+    private WisataAdapter wisataAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        wisataItems = new ArrayList<>();
+        getData();
+    }
 
-        cvAgrowisata.setOnClickListener(new View.OnClickListener() {
+    private void getData() {
+        ApiService apiService = ApiConfig.getApiService();
+        apiService.getData().enqueue(new Callback<RoodPariwisataModel>() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(),agrowisata.class));
+            public void onResponse(Call<RoodPariwisataModel> call, Response<RoodPariwisataModel> response) {
+                if (response.isSuccessful()) {
+                    wisataItems = response.body().getWisata();
+                    wisataAdapter = new WisataAdapter(wisataItems, getApplicationContext());
+                    recyclerView.setAdapter(wisataAdapter);
+                    recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<RoodPariwisataModel> call, Throwable t) {
+                Toast.makeText(main.this, "" + t.getMessage(), Toast.LENGTH_SHORT).show();
             }
         });
     }
 
     private void initView() {
-        cvAgrowisata = (CardView) findViewById(R.id.cvAgrowisata);
-        cvAlam = (CardView) findViewById(R.id.cvAlam);
-        cvBudaya = (CardView) findViewById(R.id.cvBudaya);
-        cvEdukasi = (CardView) findViewById(R.id.cvEdukasi);
-        cvKawasanWisataTerpadu = (CardView) findViewById(R.id.cvKawasanWisataTerpadu);
-        cvKolamRenang = (CardView) findViewById(R.id.cvKolamRenang);
-        cvLainLain = (CardView) findViewById(R.id.cvLainLain);
-        cvMinatKhusus = (CardView) findViewById(R.id.cvMinatKhusus);
-        cvReligi = (CardView) findViewById(R.id.cvReligi);
-        cvTaman = (CardView) findViewById(R.id.cvTaman);
+        recyclerView = findViewById(R.id.recyclerView);
     }
 }
