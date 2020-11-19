22781855cefdcae9f0ed8cb4125d3178c5cdf38b
Jefven <fernandojefven@gmail.com>
2020-11-19 12:54:09 +0700

    Notify Alarm & Nav Bar

--- app/src/main/java/com/example/wisatanusantara/main.java
+++ app/src/main/java/com/example/wisatanusantara/main.java
@@ -3,20 +3,28 @@ package com.example.wisatanusantara;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.wisatanusantara.adapter.WisataAdapter;
+import com.example.wisatanusantara.alarm.AlarmActivity;
 import com.example.wisatanusantara.model.RoodPariwisataModel;
 import com.example.wisatanusantara.model.WisataItem;
 import com.example.wisatanusantara.rest.ApiConfig;
 import com.example.wisatanusantara.rest.ApiService;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -24,10 +32,13 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
+
 public class main extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ArrayList<WisataItem> wisataItems;
     private WisataAdapter wisataAdapter;
+    private Text mAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,19 @@ public class main extends AppCompatActivity {
         initView();
         wisataItems = new ArrayList<>();
         getData();
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(main.this, AlarmActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 
     private void getData() {
@@ -60,5 +84,24 @@ public class main extends AppCompatActivity {
 
     private void initView() {
         recyclerView = findViewById(R.id.recyclerView);
+        mAlarm= (Text) findViewById(R.id.action_alarm);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_alarm:
+                Intent intent = new Intent(main.this, AlarmActivity.class);
+                String mOrderMessage = null;
+                intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+                startActivity(intent);
+                return true;
+            default:
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
